a44a6cd423651247b6a2fc27b0836dacd0bb0a83
Anton Noë <63851898+antonnoe@users.noreply.github.com>
2026-03-05 10:12:56 +0100

    Fix condition for highestStepVisited in getRapportNiveau

--- src/engine/rapport-niveau.ts
+++ src/engine/rapport-niveau.ts
@@ -4,6 +4,6 @@ export function getRapportNiveau(toolState: ToolState): 0 | 1 | 2 | 3 {
   const hasPostcode = toolState.postcode.length === 5;
   if (!hasPostcode) return 0;
   if (!toolState.huisTypeGekozen) return 1;
-  if (toolState.highestStepVisited < 3) return 2;
+  if (toolState.highestStepVisited < 2) return 2;
   return 3;
 }
